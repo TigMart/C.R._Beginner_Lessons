378d9011c33955ef4eae9de3df01533da5f13536
Tigran <tig.mar@yandex.ru>
2021-02-16 18:21:25 +0400

    Task-9

---
+++ Task_9.cpp
@@ -0,0 +1,30 @@
+﻿//Task_9: Use the four arithmetic operations with numbers 12 and 23, print the results on screen.
+
+#include <iostream>
+
+int Add(int x, int y)
+{
+    return x + y;
+}
+int Sub(int x, int y)
+{
+    return x - y;
+}
+int Mul(int x, int y)
+{
+    return x * y;
+}
+float Div(float x, float y)
+{
+    return x / y;
+}
+
+int main()
+{
+    using namespace std;
+    cout << "Add - " << Add(12, 23) << endl;
+    cout << "Sub - " << Sub(12, 23) << endl;
+    cout << "Mul - " << Mul(12, 23) << endl;
+    cout << "Div - " << Div(12, 23) << endl;
+    return 0;
+}
