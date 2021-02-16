6983613c6a55c486c33c8e06a06490f94cd0eae8
Tigran <tig.mar@yandex.ru>
2021-02-16 18:03:39 +0400

    Task-7

---
+++ Task_7.cpp
@@ -0,0 +1,24 @@
+//Task_7: Write a program that will print your name and surname on the screen in separate lines. 
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Tigran" << std::endl; 
+    std::cout << "Martirosyan" << std::endl;
+    return 0;
+}
+
+/* or
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Tigran\n";
+    cout << "Martirosyan\n";
+    return 0;
+}
+*/
+
+
