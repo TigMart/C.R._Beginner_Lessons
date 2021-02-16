f2544a6174f18eb70d407fa89cc3e36f5683e9b2
Tigran <tig.mar@yandex.ru>
2021-02-16 18:07:37 +0400

    Task-8

---
+++ Task_8.cpp
@@ -0,0 +1,15 @@
+﻿//Task_8: Write a program that will print the sums of 4 և 5 numbers on the screen. 
+
+#include <iostream>
+
+int Add(int x, int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    std::cout << Add(4, 5) << std::endl;
+    return 0;
+}
+
