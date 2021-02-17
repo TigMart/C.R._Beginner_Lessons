d78bf81929cedfaef814e30f3253565b715766e9
Tigran <tig.mar@yandex.ru>
2021-02-17 21:07:22 +0400

    Task-12

---
+++ Task_12.cpp
@@ -0,0 +1,17 @@
+// Task_12
+
+#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    std::cout << "Dear GRNO input any 3 number: " << std::endl;
+    std::cin >> a >> b >> c;
+    k = a + b + c;
+    std::cout << ;
+    return 0;
+}
+
+
