e54dfad2b2aa653707db9dd29ac783abb9ea21d8
Tigran <tig.mar@yandex.ru>
2021-02-17 19:28:38 +0400

    Task-11

---
+++ Task_11.cpp
@@ -0,0 +1,16 @@
+//Task_11: Declare variables, enter values, print on screen 
+
+
+#include <iostream>
+
+int main()
+{
+    int a;
+    char b;
+    float c;
+        
+    std::cin >> a >> b >> c;
+    std::cout << "Result: " << a << std::endl << b << std::endl << c;
+    return 0;
+}
+
