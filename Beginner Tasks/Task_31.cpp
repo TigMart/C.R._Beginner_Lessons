eb65dcd1bc0f4f22e58627cd9392057e2aba103f
Tigran <tig.mar@yandex.ru>
2021-02-25 02:27:12 +0400

    Task-31

---
+++ Beginner Tasks/Task_31.cpp
@@ -0,0 +1,22 @@
+// Task_31: 
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int num = 0;
+    std::string name;
+    std::cout << "What is your name? ";
+    std::cin >> name;
+    std::cout << "How many times do I print your name on the screen? ";
+    std::cin >> num;
+    while (num)
+    {
+        std::cout<< std::endl << name;
+        --num;
+    }
+    return 0;
+}
+
+
