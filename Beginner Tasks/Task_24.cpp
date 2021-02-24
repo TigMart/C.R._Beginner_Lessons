b991c21f1929463616b6188415300ddf60c37119
Tigran <tig.mar@yandex.ru>
2021-02-24 21:29:37 +0400

    Task-24

---
+++ Beginner Tasks/Task_24.cpp
@@ -0,0 +1,20 @@
+// Task_24: Print only even numbers in the 0-100 range 
+
+
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    int b = 2;
+    while (a <= 100) {
+        a += 1;
+        if (a % b == 0) {
+            std::cout << a<<std::endl;
+            
+        }
+    }
+    return 0;
+}
+
+
