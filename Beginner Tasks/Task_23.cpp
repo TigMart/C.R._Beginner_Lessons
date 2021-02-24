942268f9e8c377f434e5d778baec20ca99acd1ed
Tigran <tig.mar@yandex.ru>
2021-02-24 21:27:04 +0400

    Task-23

---
+++ Beginner Tasks/Task_23.cpp
@@ -0,0 +1,14 @@
+// Task_23: print numbers 100 - 0
+
+#include <iostream>
+
+int main()
+{
+    int a = 100;
+    while (a >= 0) {
+        std::cout << a << std::endl;
+        a -= 1;
+    }
+    return 0;
+    
+}
