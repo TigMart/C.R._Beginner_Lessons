397aca5b4a2a7eeec23aefb38ccf265c2b6c1d7e
Tigran <tig.mar@yandex.ru>
2021-02-24 21:32:13 +0400

    Task-26

---
+++ Beginner Tasks/Task_26.cpp
@@ -0,0 +1,55 @@
+// Task_26 : Declare Boolean variables with different values. Print the results of "AND" & "OR" operations on the screen. 
+
+
+#include <iostream>
+
+int main()
+{
+
+	/*int a = 4;
+	int b = 3;
+	bool c=(a - b == 2 && b - a == -2);
+	bool d=(a - b == 2 && b - a == -1);
+	bool e=(a - b == 1 && b - a == -2);
+	bool f=(a - b == 1 && b - a == -1);
+	bool c1=(a - b == 2 || b - a == -2);
+	bool d1=(a - b == 2 || b - a == -1);
+	bool e1=(a - b == 1 || b - a == -2);
+	bool f1=(a - b == 1 || b - a == -1);
+
+
+
+	std::cout << "|___" << "A___|___" << "B___|" <<"__AND__|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "0___|___" << "0___|" << "___" << c << "___|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "0___|___" << "1___|" << "___" << d << "___|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "1___|___" << "0___|" << "___" << e << "___|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "1___|___" << "1___|" << "___" << f << "___|" << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "A___|___" << "B___|" <<"___OR__|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "0___|___" << "0___|" << "___" << c1 << "___|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "0___|___" << "1___|" << "___" << d1 << "___|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "1___|___" << "0___|" << "___" << e1 << "___|" << std::endl;
+	std::cout << std::endl;
+	std::cout << "|___" << "1___|___" << "1___|" << "___" << f1 << "___|" << std::endl;
+	return 0;
+	/*int a = 0;
+	int b = 0;
+	int c = 0;
+	std::cin >> a;
+	std::cin >> b;
+	c = a & b;
+	std::cout << c;*/
+	double f = 25. / 2;
+	std::cout << f;
+
+}
+
+
