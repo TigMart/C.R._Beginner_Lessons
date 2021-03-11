cd4b271a5bd26955377e1928b7513034428b40ac
Tigran <77511200+TigMart@users.noreply.github.com>
2021-03-11 15:57:06 +0400

    Problem-9

--- Problems_II/Problem_9.cpp
+++ Problems_II/Problem_9.cpp
@@ -11,14 +11,17 @@ int main()
 
     for (int i = 0; i < name_in.size(); ++i)
     {
-        if (i % 2 != 0)
+        if (name_in[i] >= 97 && name_in[i] <= 122)
         {
-            name_in[i] -= 32;
-            name_out.push_back(name_in[i]);
-        }
-        else
-        {
-            name_out.push_back(name_in[i]);
+            if (i % 2 != 0)
+            {
+                name_in[i] -= 32;
+                name_out.push_back(name_in[i]);
+            }
+            else
+            {
+                name_out.push_back(name_in[i]);
+            }
         }
     }
     std::cout << "every second symbol in Upppercase: " << name_out;
