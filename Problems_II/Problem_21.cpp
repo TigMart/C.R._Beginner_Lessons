69bff942aeb1dfa7b18663796909281591fedb87
Tigran <tig.mar@yandex.ru>
2021-03-21 22:49:25 +0400

    Problem-21

---
+++ Problems_II/Problem_21.cpp
@@ -0,0 +1,34 @@
+// Task_21.cpp :
+
+#include <iostream>
+#include <new>
+int main()
+{
+    int arr_size = 0;
+    std::cout << "Enter a size of array: ";
+    std::cin >> arr_size;
+    int* arr_x = new (std::nothrow)int[arr_size];
+    int* arr_y = new (std::nothrow)int[arr_size];
+    if (arr_x == nullptr || arr_y == nullptr) {
+        std::cout << "Error: Allocated memory. ";
+    }
+    else {
+        for (int i = 0; i < arr_size; ++i) {
+            std::cout << "Elements of arr_x " << i << ": ";
+            std::cin >> arr_x[i];
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < arr_size; ++i) {
+            std::cout << "Elements of arr_y " << i << ": ";
+            std::cin >> arr_y[i];
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < arr_size; ++i) {
+            std::cout << "ADD of (index -> x: " << i << " + " << "y: " << i << ")\nVALUE:  (" << arr_x[i]
+                << " + " << arr_y[i] << ") = " << arr_x[i] + arr_y[i] << std::endl << std::endl;
+        }
+        delete[] arr_x;
+        delete[] arr_y;
+    }
+    return 0;
+}
