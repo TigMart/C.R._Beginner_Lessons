08028e7b0eadb8e4bbd676ad24a1c0f3395fbbef
Tigran <tig.mar@yandex.ru>
2021-03-21 22:47:59 +0400

    Problem-19

---
+++ Problems_II/Problem_19.cpp
@@ -0,0 +1,26 @@
+// Task_19.cpp :
+
+#include <iostream>
+#include <new>
+int main()
+{
+    int arr_size = 0;
+    std::cout << "Enter a size of array: ";
+    std::cin >> arr_size;
+    int* arr = new (std::nothrow) int[arr_size];
+    if (arr == nullptr) {
+        std::cout << "Error: Allocated Memroy";
+        return 0;
+    }
+    else {
+        for (int i = 0; i < arr_size; ++i) {
+            std::cout << "Element " << i << ": ";
+            std::cin >> arr[i];
+        }
+        for (int i = 0; i < arr_size; ++i) {
+            std::cout << arr[(arr_size - 1) - i] << " ";
+        }
+    }
+    delete[] arr;
+    return 0;
+}
