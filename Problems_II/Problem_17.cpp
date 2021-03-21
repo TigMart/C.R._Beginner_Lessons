f2de3384b7690134faa61078a9417e2273fb36d7
Tigran <tig.mar@yandex.ru>
2021-03-21 22:46:18 +0400

    Problem-17

---
+++ Problems_II/Problem_17.cpp
@@ -0,0 +1,31 @@
+// Task_17.cpp : 
+
+#include <iostream>
+#include <new>
+
+int main()
+{
+    int arr_size = 0;
+    int min_index = 0;
+    std::cout << "Enter a size of array: ";
+    std::cin >> arr_size;
+    int* arr = new (std::nothrow) int[arr_size];
+    if (arr == nullptr) {
+        std::cout << "Error: Allocated Mermory: ";
+        return 0;
+    }
+    else {
+        for (int i = 0; i < arr_size; ++i)
+        {
+            std::cout << "Enter a number " << i << ": ";
+            std::cin >> arr[i];
+            if (arr[min_index] > arr[i])
+            {
+                min_index = i;
+            }
+        }
+        std::cout << "The smallest element index: " << min_index; 
+    }
+    delete[] arr;
+    return 0; 
+}
