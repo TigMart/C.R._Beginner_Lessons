c36c7ec74e0de874c8bd4966edd7031df6c3a95b
Tigran <77511200+TigMart@users.noreply.github.com>
2021-03-23 09:32:18 +0400

    Problem-13

--- Problems_II/Problem_13.cpp
+++ Problems_II/Problem_13.cpp
@@ -3,8 +3,7 @@
 #include <iostream>
 int main() 
 {
-	int maximum = 9;
-	int min_num = 0;
+	int min_num = 9;
 	int num_in = 0;
 	int num1 = 0;
 	int max_num = 0;
@@ -15,7 +14,7 @@ int main()
 		if (num1 > max_num) {
 			max_num = num1;
 		}
-		if (maximum > num1) {
+		if (min_num > num1) {
 			min_num = num1;
 		}
 		num_in /= 10;
