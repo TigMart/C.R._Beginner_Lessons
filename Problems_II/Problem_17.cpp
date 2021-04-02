33f7c2403452ac12637b8f5f9f3d11f9a5daf2bd
Tigran <77511200+TigMart@users.noreply.github.com>
2021-04-02 16:07:28 +0400

    Problem-17

--- Problems_II/Problem_17.cpp
+++ Problems_II/Problem_17.cpp
@@ -9,6 +9,7 @@ int main()
     int min_index = 0;
     std::cout << "Enter a size of array: ";
     std::cin >> arr_size;
+    if (arr_size == 0) { return 0; }
     int* arr = new (std::nothrow) int[arr_size];
     if (arr == nullptr) {
         std::cout << "Error: Allocated Mermory: ";
