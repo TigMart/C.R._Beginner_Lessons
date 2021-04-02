daa92755b00ffc8bd8c8ebd372a4a3e9f8ba9a3c
Tigran <77511200+TigMart@users.noreply.github.com>
2021-04-02 14:18:39 +0400

    Problem-1

--- Problems_II/Problem_1.cpp
+++ Problems_II/Problem_1.cpp
@@ -1,30 +1,29 @@
 // Task_1: Split
 
-/*
 #include <iostream>
 
 int main()
 {
     int number = 0;
-    int i = 0;
+    int count = 0;
     int reverse = 0;
     std::cout << "Enter a positive number = ";
     std::cin >> number;
-    while (number > 0)
+    while (number)
     {
-        i = number % 10;
-        reverse = reverse * 10 + i;
+        ++count;
+        reverse = reverse * 10 + (number % 10);
         number /= 10;      
     }
-    while (reverse > 0)
+    
+    while (count--)
     {
         std::cout <<"'"<< reverse % 10 <<"'"<< " ";
         reverse /= 10;
     }
 }
-*/
 
-#include <iostream>
+/*#include <iostream>
 
 void numbers(int a); 
 
@@ -44,4 +43,4 @@ void numbers(int a)
         numbers(a / 10);
         std::cout << "'" << a % 10 << "'" << std::endl;
     }
-}
+}*/
