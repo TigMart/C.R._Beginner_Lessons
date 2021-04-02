72dd7787051f34b73b3d4668185ba3308f634e1f
Tigran <77511200+TigMart@users.noreply.github.com>
2021-04-02 17:19:13 +0400

    Problem-23

--- Problems_II/Problem_23.cpp
+++ Problems_II/Problem_23.cpp
@@ -5,39 +5,36 @@
 
 int main()
 {
-    int row_size = 0;
-    int col_size = 0;
-    std::cout << "Enter a size of row: ";
-    std::cin >> row_size;
-    std::cout << "Enter a size of column: ";
-    std::cin >> col_size;
-    int** matrix = new (std::nothrow) int* [row_size];
+    int size = 0;
+    std::cout << "Enter a size: ";
+    std::cin >> size;
+    int** matrix = new (std::nothrow) int* [size];
     if (matrix == nullptr) {
         std::cout << "Error: Allocated memory ";
         return 0;
     }
     else
     {
-        for (int i = 0; i < row_size; ++i) {
+        for (int i = 0; i < size; ++i) {
 
-            matrix[i] = new int[col_size];
+            matrix[i] = new int[size];
         }
-        for (int i = 0; i < row_size; ++i) {
-            for (int j = 0; j < col_size; ++j) {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
                 matrix[i][j] = rand() % 90 + 10;
             }
         }
-        for (int i = 0; i < row_size; ++i) {
+        for (int i = 0; i < size; ++i) {
             std::cout << std::endl;
-            for (int j = 0; j < col_size; ++j) {
+            for (int j = 0; j < size; ++j) {
                 std::cout << matrix[i][j] << "  ";
             }
         }
         std::cout << std::endl << std::endl;
-        for (int i = 0; i < row_size; ++i) {
-            std::cout << "The auxiliary diagonal elements of a matrix: " << i << " x " << row_size - i - 1 << " = " << matrix[i][row_size-i-1] << std::endl;
+        for (int i = 0; i < size; ++i) {
+            std::cout << "The auxiliary diagonal elements of a matrix: " << i << " x " << size - i - 1 << " = " << matrix[i][size-i-1] << std::endl;
         }
-        for (int i = 0; i < row_size; i++) {
+        for (int i = 0; i < size; i++) {
             delete[] matrix[i];
         }
         delete[] matrix;
