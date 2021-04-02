0df931c768b1b65aa9ec16b872effe42a1294286
Tigran <77511200+TigMart@users.noreply.github.com>
2021-04-02 14:42:07 +0400

    Problem-13

--- Problems_II/Problem_13.cpp
+++ Problems_II/Problem_13.cpp
@@ -3,23 +3,23 @@
 #include <iostream>
 int main() 
 {
+	int max_num = 0;
 	int min_num = 9;
 	int num_in = 0;
-	int num1 = 0;
-	int max_num = 0;
+	int num = 0;
 	std::cout << "Enter a number: ";
 	std::cin >> num_in;
-	while (num_in) {
-		num1 = num_in % 10;
-		if (num1 > max_num) {
-			max_num = num1;
+	while (num_in) 
+	{
+		num = num_in % 10;
+		if (num > max_num) {
+			max_num = num;
 		}
-		if (min_num > num1) {
-			min_num = num1;
+		if (min_num > num) {
+			min_num = num;
 		}
 		num_in /= 10;
 	}
 	std::cout << "min number is: " << min_num << std::endl << "max number is: " << max_num;
 	return 0;
 }
-
