33cad2e913c5e162890d9ce3460da25825659343
Tigran <77511200+TigMart@users.noreply.github.com>
2021-04-03 19:46:25 +0400

    Problem-32

--- Problems_II/Problem_32.cpp
+++ Problems_II/Problem_32.cpp
@@ -3,34 +3,45 @@
 #include <iostream>
 #include <string>
 
-std::string Reapeted_char(std::string text);
+void Reapeted_char(std::string text);
 
 int main() {
-    std::cout << Reapeted_char("hheeellowoooooooorld");
+    std::string str;
+    getline(std::cin, str, '|');
+    std::cin.get();
+    Reapeted_char(str);
     return 0;
 }
 
-std::string Reapeted_char(std::string text) {
+void Reapeted_char(std::string text) {
     int max = 0;
     int count = 0;
     std::string maxCharacter;
     int size = text.length();
-
+    int tmp;
         for (int i = 0; i < size; i++) {
-            int tmp = 1;
+            tmp = 1;
             if (text[i] == text[i + 1]) {
-                for (int j = i + 1; j < size; ++j) {
+                for (int j = i + 1;; ++j) {
                     if (text[j] == text[j + 1]) {
                         tmp++;
                         i++;
+                    }
+                    else
+                    {
+                        break;
                     }
                 }
             }
             if (tmp > count)
             {
                 count = tmp;
-                maxCharacter = text[i];
+                maxCharacter =  text[i];
             }
         }
-    return maxCharacter;
+        while (count > 0)
+        {
+            std::cout << maxCharacter;
+            --count;
+        }
 }
