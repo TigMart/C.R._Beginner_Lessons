5da09163c4afee1dad8e07ff1ef46089f6769709
Tigran <77511200+TigMart@users.noreply.github.com>
2021-04-03 12:23:12 +0400

    Problem-27

---
+++ Problems_II/Problem_27/Problem_27.cpp
@@ -0,0 +1,34 @@
+// Task_27.cpp : 
+
+#include "Functions.h"
+
+int main()
+{
+   Spilt_number(458);
+   Reverse(123);
+   std::cout << Sum_of_integer(123) << std::endl;
+   std::cout << Mul_of_integer(1234)<< std::endl;
+   Calculator(4, 200, '*');
+   Str_reverse("Tigran");
+   Name_without_vowels("Tigran");
+   Name_in_uppercase("tigran");
+   Ev_sec_sym_in_Upp("tigran");
+   Convert_m_to_km(1000);
+   Print_Home(10);
+   Print_triangle(10);
+   Min_Max(654321);
+   Convert_Dec_to_Hex(255);
+   Convert_Dec_to_Bin(255);
+   Max_index_arr();
+   Min_index_arr();
+   Sum_Min_Max_arr(5);
+   Reverse_array(4);
+   Mul_Array_elem(6);
+   Add_Array_elem(5);
+   Principal_diagonal_matrix(10);
+   Secondary_diagonal_matrix(10);
+   Sum_Diagonals_matrix(10);
+   Swap_Diagonals_Matrix(8);
+   Quadratic_equation();
+   return 0;
+}
