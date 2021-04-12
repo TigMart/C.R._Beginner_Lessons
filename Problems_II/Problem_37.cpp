c53241322ce4218ab0d029b0ab5e18dbc7cf5ab0
Tigran <tig.mar@yandex.ru>
2021-04-12 21:06:07 +0400

    Problem-37

---
+++ Problems_II/Problem_37.cpp
@@ -0,0 +1,20 @@
+// Problem_37.cpp:
+
+#include <iostream>
+
+struct Point
+{
+    int x;
+    int y;
+
+}p1;
+
+int main()
+{
+    std::cout << "Please enter X = ";
+    std::cin >> p1.x;
+    std::cout << "Please enter Y = ";
+    std::cin >> p1.y;
+    return 0;
+}
+
