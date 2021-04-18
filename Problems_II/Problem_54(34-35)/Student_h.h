2385b730361a6111fd88d690f8c9d7bdce1f1a99
Tigran <77511200+TigMart@users.noreply.github.com>
2021-04-19 00:10:34 +0400

    Student_h.h

---
+++ Problems_II/Problem_54(34-35)/Student_h.h
@@ -0,0 +1,28 @@
+#ifndef Student_h
+#define Student_h
+
+#include <string>
+#include <iostream>
+
+class Student 
+{
+    public:
+        Student();
+        ~Student();
+    public:
+        void set_name();
+        std::string get_name()const;        
+        void set_surname();
+        std::string get_surname()const;
+        void set_age();
+        unsigned int get_age()const;
+        void set_lessons_name(/*const std::string& lessons_name*/);
+        std::string get_lessons_name()const;
+
+    private:
+        std::string name;
+        std::string surname;
+        unsigned int age;
+        std::string lessons_name;
+};
+#endif
