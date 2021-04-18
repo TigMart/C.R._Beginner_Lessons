12cbb53dc7ac0f70e239a51381ec0d1e944c8fbb
Tigran <77511200+TigMart@users.noreply.github.com>
2021-04-19 00:11:53 +0400

    StudentMain.cpp

---
+++ Problems_II/Problem_54(34-35)/StudentMain.cpp
@@ -0,0 +1,31 @@
+#include "Student_h.h"
+
+int main() 
+{
+	Student male;
+	Student female;
+	male.set_name();
+	male.set_surname();
+	male.set_age();
+	male.set_lessons_name();
+	female.set_name();
+	female.set_surname();
+	female.set_age();
+	female.set_lessons_name();
+	std::cout << "Problem 34: "<<std::endl;
+	std::cout << male.get_name() << std::endl;
+	std::cout << male.get_surname() << std::endl;
+	std::cout << male.get_age() << std::endl;
+	std::cout << male.get_lessons_name() << std::endl;	
+	std::cout << female.get_name() << std::endl;
+	std::cout << female.get_surname() << std::endl;
+	std::cout << female.get_age() << std::endl;
+	std::cout << female.get_lessons_name() << std::endl;
+	std::cout << std::endl << "Problem 35) Name of older: " << std::endl;
+	if (male.get_age() >= female.get_age()) {
+		std::cout << male.get_name();
+	}
+	else {
+		std::cout << female.get_name();
+	}
+}
