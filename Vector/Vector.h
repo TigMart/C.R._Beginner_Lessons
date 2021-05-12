7399507a7eb01781c7f2d906e318e3d1800bf1b9
Tigran <77511200+TigMart@users.noreply.github.com>
2021-05-13 01:35:12 +0400

    Vector.h

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -2,33 +2,260 @@
 #define _VECTOR_
 
 #include <iostream>
+#include <stdexcept>
 
 template <typename T>
 class Vector
 {
 public:
-	Vector();
-	~Vector();
-	Vector<T>& operator=(const Vector<T>& rhs);
+	Vector();  // default ctor
+	Vector(const Vector<T>& other); // copy ctor
+	Vector<T>& operator=(const Vector<T>& rhs); // copy assignment
+	Vector(Vector<T>&& rvalue)noexcept; // move ctor 
+	Vector<T>& operator=(Vector<T>&& rv)noexcept; // move assignment
+	Vector<T> operator+(const Vector<T>& oth); // operator+
+	~Vector(); // dtor
 
 	void push_back(const T& elem);
-	void push(int elem, int index);
+	void push(const T& elem, int index);
 	T& operator[](int index)const;
 	void pop_Back();
 	int size()const;
-	void print();
+	int capacity()const;
 	void push_front(const T& elem);
 	void erase(int index);
-	short Id();
+	T& at(int index) const;
+	T& front();
+	T& back();
+	void print();
 private:
 	void ReAlloc();
-
+	void copy_vec(const Vector<T>& other);
 	int m_capacity;
 	int m_size;
 	T* m_arr;
-
 };
 
-#include "Vector_imp.tpp"
+//Implementation
+
+template <typename T>
+Vector<T>::Vector() : m_capacity{ 0 }, m_size{ 0 }, m_arr{ nullptr }
+{	
+	std::cout << "default ctor " << std::endl;
+}
+
+template <typename T>
+void Vector<T>::copy_vec(const Vector<T>& other)
+{
+	m_capacity = other.m_capacity;
+	m_size = other.m_size;
+	m_arr = new T[m_capacity];
+	for (int i = 0; i < m_size; ++i) {
+		m_arr[i] = other.m_arr[i];
+	}
+}
+
+template <typename T>
+Vector<T>::Vector(const Vector<T>& other)
+{
+	std::cout << "copy ctor " << std::endl;
+	copy_vec(other);
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& rhs)
+{
+	std::cout << "copy assignment" << std::endl;
+	if (this == &rhs) {
+		std::cout << "inqnaveragrum" << std::endl;
+		return *this;
+	}
+	delete[] m_arr;
+	m_arr = nullptr;
+	copy_vec(rhs);
+	return *this;
+}
+
+template <typename T>
+Vector<T>::Vector(Vector<T>&& rvalue) noexcept
+{
+	std::cout << "Move ctor" << std::endl;
+	m_capacity = rvalue.m_capacity;
+	m_size = rvalue.m_size;
+	m_arr = rvalue.m_arr;
+	rvalue.m_arr = nullptr;
+	rvalue.m_capacity = 0;
+	rvalue.m_size = 0;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator=(Vector<T>&& rv) noexcept
+{
+	std::cout << "Move assignment" << std::endl;
+	if (this == &rv) {
+		std::cout << "inqnaveragrum" << std::endl;
+		return *this;
+	}
+	m_capacity = rv.m_capacity;
+	m_size = rv.m_size;
+	m_arr = rv.m_arr;
+	rv.m_arr = nullptr;
+	rv.m_capacity = 0;
+	rv.m_size = 0;
+	return *this;
+}
+
+template<typename T>
+Vector<T> Vector<T>::operator+(const Vector<T>& oth)
+{
+	std::cout << "operator + " << std::endl;
+	Vector<T> result;
+	result.m_capacity = m_capacity + oth.m_capacity;
+	result.m_size = m_size + oth.m_size;
+	result.m_arr = new T[result.m_capacity];
+	int j = 0;
+	for (int i = 0; i < result.m_size; ++i) {
+		if (i < m_size) {
+			result.m_arr[i] = m_arr[i];
+		}
+		else {
+			result.m_arr[i] = oth.m_arr[j];
+			++j;
+		}
+	}
+	return result;
+}
+
+template <typename T>
+void Vector<T>::push_back(const T& elem)
+{
+	if (m_size == m_capacity) {
+		ReAlloc();
+	}
+	m_arr[m_size++] = elem;
+}
+
+template <typename T>
+void Vector<T>::push(const T& elem, int index)
+{
+	if (index == m_capacity)
+		push_back(elem);
+
+	else
+		m_arr[index] = elem;
+}
+
+template <typename T>
+T& Vector<T>::operator[](int index)const
+{
+	return m_arr[index];
+}
+
+template <typename T>
+void Vector<T>::pop_Back()
+{
+	if (m_size > 0) {
+		m_size--;
+	}
+}
+
+template <typename T>
+int Vector<T>::size() const { return m_size; }
+
+template <typename T>
+int Vector<T>::capacity() const { return m_capacity; }
+
+
+template<typename T>
+T& Vector<T>::front()
+{
+	return m_arr[0];
+}
+
+template<typename T>
+T& Vector<T>::back()
+{
+	return m_arr[size() - 1];
+}
+
+template<typename T>
+void Vector<T>::push_front(const T& elem) {
+	if (m_size == m_capacity) {
+
+		m_capacity *= 2;
+		T* temp = new T[m_capacity];
+		temp[0] = elem;
+		for (int i = 1; i < m_size; ++i) {
+			temp[i] = m_arr[i];
+		}
+		delete[] m_arr;
+		m_arr = temp;
+		++m_size;
+	}
+	else if (m_size < m_capacity) {
+		for (int i = m_size; i > 0; --i) {
+			m_arr[i] = m_arr[i - 1];
+		}
+		m_arr[0] = elem;
+		++m_size;
+	}
+}
+
+template <typename T>
+void Vector<T>::erase(int index)
+{
+	T* tmp = new T[m_capacity];
+	for (int i = 0; i < index; ++i) {
+		tmp[i] = m_arr[i];
+	}
+	for (int i = index + 1; i < m_size; ++i) {
+		tmp[i - 1] = m_arr[i];
+	}
+	m_size--;
+	delete[] m_arr;
+	m_arr = nullptr;
+	m_arr = tmp;
+}
+
+template <typename T>
+T& Vector<T>::at(int index) const
+{
+	if (index <= m_size)
+		return m_arr[index];
+	std::cout << "Out of range! " << std::endl;
+	throw 0;
+
+}
+
+template <typename T>
+void Vector<T>::ReAlloc()
+{
+	if (m_capacity == m_size)
+	{
+		m_capacity = m_capacity ? m_capacity * 2 : 1;
+		T* temp = new T[m_capacity];
+		for (int i = 0; i < m_size; ++i) {
+			temp[i] = m_arr[i];
+		}
+		delete[] m_arr;
+		m_arr = temp;
+	}
+}
+
+template <typename T>
+void Vector<T>::print()
+{
+	for (int i = 0; i < m_size; ++i) {
+		std::cout << m_arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+template <typename T>
+Vector<T>::~Vector()
+{
+	std::cout << "Dtor" << std::endl;
+	delete[] m_arr;
+}
 
 #endif // _VECTOR_
