e8a85c3890a377006d0567b5443fd542b45ab296
Tigran <77511200+TigMart@users.noreply.github.com>
2021-05-20 18:45:42 +0400

    Stack.h

--- Vector/Stack.h
+++ Vector/Stack.h
@@ -2,9 +2,8 @@
 #define _STACK_
 #include "Vector.h"
 
-
 template <class T>
-class Stack : private Vector<T>
+class Stack : private Vector<int>
 {
 public:
 	void push(const T& elem);
@@ -14,11 +13,10 @@ private:
 	size_t m_sp{ 0 };
 };
 
-
 template<class T>
 void Stack<T>::push(const T& elem)
 {
-	Vector<T>::push_back(elem);
+	push_back(elem);
 	++m_sp;
 }
 
@@ -26,13 +24,13 @@ template<class T>
 T& Stack<T>::pop()
 {
 	--m_sp;
-	return Vector<T>::at(m_sp);
+	return at(m_sp);
 }
 
 template<class T>
 void Stack<T>::print() 
 {
-	T* tmp = Vector<T>::get_arr();
+	T* tmp = get_arr();
 	for (int i = 0; i < m_sp; ++i) {
 		std::cout << tmp[i] << " ";
 	}
